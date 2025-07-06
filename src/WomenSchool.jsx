054cac1836438a87bf28a213c3b1ee7198d96d29
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-06 13:48:03 +0800

    Minor modifications

--- src/WomenSchool.jsx
+++ src/WomenSchool.jsx
@@ -1,14 +1,24 @@
-import school1 from './assets/school/school1.png';
-import school2 from './assets/school/school2.png';
-import school3 from './assets/school/school3.png';
-import school4 from './assets/school/school4.png';
+import school1 from './assets/women/school/school1.png';
+import school2 from './assets/women/school/school2.png';
+import school3 from './assets/women/school/school3.png';
+import school4 from './assets/women/school/school4.png';
 
-function Products () {
+import stilettos1 from './assets/women/stilettos/stilettos1.png';
+import stilettos2 from './assets/women/stilettos/stilettos2.png';
+import stilettos3 from './assets/women/stilettos/stilettos3.png';
+import stilettos4 from './assets/women/stilettos/stilettos4.png';
+
+import block1 from './assets/women/block/block1.png';
+import block2 from './assets/women/block/block2.png';
+import block3 from './assets/women/block/block3.png';
+import block4 from './assets/women/block/block4.png';
+
+function WomenSchool () {
     return (
         <>
         <div className="productsContainer">
             <p className="productCategory">
-                Category
+                School Shoes
             </p>
                 <div className="productItem1">
                     <img 
@@ -55,8 +65,108 @@ function Products () {
                     </p>
                 </div>
             </div>
+
+            <div className="productsContainer">
+            <p className="productCategory">
+                Stilettos
+            </p>
+                <div className="productItem1">
+                    <img 
+                        src={stilettos1} 
+                        alt=""
+                        className="productImage1" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem2">
+                    <img 
+                        src={stilettos2}
+                        alt="" 
+                        className="productImage2" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem3">
+                    <img 
+                        src={stilettos3}
+                        alt="" 
+                        className="productImage3" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem4">
+                    <img 
+                        src={stilettos4}
+                        alt="" 
+                        className="productImage4" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+            </div>
+
+            <div className="productsContainer">
+            <p className="productCategory">
+                Block heels
+            </p>
+                <div className="productItem1">
+                    <img 
+                        src={block1} 
+                        alt=""
+                        className="productImage1" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem2">
+                    <img 
+                        src={block2}
+                        alt="" 
+                        className="productImage2" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem3">
+                    <img 
+                        src={block3}
+                        alt="" 
+                        className="productImage3" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+                <div className="productItem4">
+                    <img 
+                        src={block4}
+                        alt="" 
+                        className="productImage4" 
+                    />
+                    <p className="productDescription">
+                        $XX,XXX <br />
+                        x inch
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
 
-export default Products;
+export default WomenSchool;
